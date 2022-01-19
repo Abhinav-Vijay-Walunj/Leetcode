45569ce3f60770d67d202128b2fffbe2b0f14283
Abhinav-Vijay-Walunj <64826301+Abhinav-Vijay-Walunj@users.noreply.github.com>
2022-01-19 19:30:30 +0530

    Time: 15 ms (26.82%), Space: 7.6 MB (73.74%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -21,12 +21,12 @@ public:
         while(fast->next !=NULL && fast->next->next!=NULL)
         {
             slow=slow->next;
-            fast=fast->next->next;
+            fast=fast->next->next;//traverse the list until we didn't get cycle or we end up to null
             if(fast == slow)
             {
                 while(slow != t)
                 {
-                    slow=slow->next;
+                    slow=slow->next;//when cycle is present then we start from head and we check if we get a slow and temp equal.
                     t=t->next;
                 }
                 return t;
